81b5655718039edebad884e2a9c1c03ce96dc433
Natalia Rudnicka <rudnicka.natalia45@gmail.com>
2022-10-23 09:30:09 +0200

    Zmiana opisu w jednej z funkcji

--- src/car.cpp
+++ src/car.cpp
@@ -3,7 +3,7 @@
 string Auto(string marka, string model)
 {
 if (marka == model)
-    {return "Nieprawidlowe dane. Wprowadzono podwojnie marke/model. Sprawdz dane!";}
+    {return "Nieprawidlowe dane. Wprowadzono podwojnie marke/model. Upewnij sie, ze wprowadziles poprawne dane!";}
 else
  {
  cout << "Twoje auto to: " << marka << " " << model << endl;
